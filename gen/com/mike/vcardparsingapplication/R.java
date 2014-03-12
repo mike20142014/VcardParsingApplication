75b02cf9bdb977b07631afbe5c7387e62913b588
mike20132013 <michael.tirkey2012@yahoo.com>
2014-03-12 13:41:08 -0400

    Lot of UI Changes. Added message icon to the send text's.

--- gen/com/mike/vcardparsingapplication/R.java
+++ gen/com/mike/vcardparsingapplication/R.java
@@ -38,8 +38,9 @@ public final class R {
         public static final int gradient=0x7f02000a;
         public static final int header_gradient=0x7f02000b;
         public static final int ic_launcher=0x7f02000c;
-        public static final int rectangle=0x7f02000d;
-        public static final int textview_gradient=0x7f02000e;
+        public static final int messageicon=0x7f02000d;
+        public static final int rectangle=0x7f02000e;
+        public static final int textview_gradient=0x7f02000f;
     }
     public static final class id {
         public static final int OR=0x7f090015;
